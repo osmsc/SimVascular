0b8075ddc01ddb6096ff320b14003bac3f26964c
nwilson <nwilson@osmsc.com>
2016-12-02 00:42:20 -0800

    BUILD: fixing bug in quick build script

--- BuildWithMake/quick-build-windows.sh
+++ BuildWithMake/quick-build-windows.sh
@@ -10,19 +10,19 @@ mkdir -p $EXTERNALS_TOP/tarfiles
 mkdir -p $EXTERNALS_TOP/bin/msvc-12.5/x64
 
 pushd $EXTERNALS_TOP/tarfiles
-wget $PARENT_URL/linux.msvc-12.5.x64.freetype-2.6.3-BUILD2016-09-05.tar.gz
-wget $PARENT_URL/linux.msvc-12.5.x64.gdcm-2.6.1-BUILD2016-09-05.tar.gz
-wget $PARENT_URL/linux.msvc-12.5.x64.itk-4.7.1-BUILD2016-09-05.tar.gz
-wget $PARENT_URL/linux.msvc-12.5.x64.mitk-2016.03-BUILD2016-11-29.tar.gz
-wget $PARENT_URL/linux.msvc-12.5.x64.mmg-5.1.0-BUILD2016-11-29.tar.gz
-wget $PARENT_URL/linux.msvc-12.5.x64.opencascade-7.0.0-BUILD2016-09-05.tar.gz
-wget $PARENT_URL/linux.msvc-12.5.x64.python-2.7.11-BUILD2016-09-05.tar.gz
-wget $PARENT_URL/linux.msvc-12.5.x64.tcltk-8.6.4-BUILD2016-09-05.tar.gz
-wget $PARENT_URL/linux.msvc-12.5.x64.vtk-6.2.0-BUILD2016-09-05.tar.gz
+wget $PARENT_URL/windows.msvc-12.5.x64.freetype-2.6.3-BUILD2016-09-05.tar.gz
+wget $PARENT_URL/windows.msvc-12.5.x64.gdcm-2.6.1-BUILD2016-09-05.tar.gz
+wget $PARENT_URL/windows.msvc-12.5.x64.itk-4.7.1-BUILD2016-09-05.tar.gz
+wget $PARENT_URL/windows.msvc-12.5.x64.mitk-2016.03-BUILD2016-09-05.tar.gz
+wget $PARENT_URL/windows.msvc-12.5.x64.mmg-5.1.0-BUILD2016-09-05.tar.gz
+wget $PARENT_URL/windows.msvc-12.5.x64.opencascade-7.0.0-BUILD2016-09-05.tar.gz
+wget $PARENT_URL/windows.msvc-12.5.x64.python-2.7.11-BUILD2016-09-05.tar.gz
+wget $PARENT_URL/windows.msvc-12.5.x64.tcltk-8.6.4-BUILD2016-09-05.tar.gz
+wget $PARENT_URL/windows.msvc-12.5.x64.vtk-6.2.0-BUILD2016-09-05.tar.gz
 popd
 
 pushd $EXTERNALS_TOP/bin/msvc-12.5/x64
-for i in $EXTERNALS_TOP/tarfiles/linux.msvc-12.5.x64.*.tar.gz; do
+for i in $EXTERNALS_TOP/tarfiles/windows.msvc-12.5.x64.*.tar.gz; do
     tar xvzf $i
 done
 popd
