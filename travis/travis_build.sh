bcb47e5254eef7bf2b2d6cfbcb2a0b3144bf9652
osmsc <nwilson@osmsc.com>
2019-06-19 19:34:27 -0700

    DEV: still trying to fix windows build

--- travis/travis_build.sh
+++ travis/travis_build.sh
@@ -43,6 +43,7 @@ if $WITH_CMAKE; then
      source $SCRIPTS/travis_cmake_build.sh
   elif [[ "$TRAVIS_OS_NAME" == "windows" ]]
   then
+     echo "cmd /c $SCRIPTS/travis_cmake_windows.bat $SV_EXTERNALS_VERSION_NUMBER \"$SV_EXTERNALS_BUILD_DIR\" \"$SV_EXTERNALS_BIN_DIR\""
      cmd /c $SCRIPTS/travis_cmake_windows.bat $SV_EXTERNALS_VERSION_NUMBER \"$SV_EXTERNALS_BUILD_DIR\" \"$SV_EXTERNALS_BIN_DIR\"
   fi
 else
